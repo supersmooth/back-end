ae94bda756c783fef1ccee5cb62fee8f95908135
robotmayo <robotmayo@gmail.com>
2014-08-07 18:16:16 -0400

    Moved db to own file. Added global layout

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ var express = require('express')
 , io = require('socket.io')(http)
 , morgan = require('morgan')
 , cookieParser = require('cookie-parser')
-, mongoose = require('mongoose')
 , bodyParser = require('body-parser')
 , passport = require('passport')
 , session = require('express-session')
 , flash = require('connect-flash')
 , hbs = require('hbs')
+, db = require('./db')
+, routes = require('./routes')
+, sockets = require('./sockets.js')
 
 // db
-mongoose.connect('mongodb://testing:testing@ds053419.mongolab.com:53419/supersmooth', function(err) {
-    if (err) console.log(err)
-    else console.log('connected to db')
+db.connect(function(err){
+    if(err) return console.error("Couldn't connect to database!")
+    console.info("Connect to database successfully")
 })
 
 require('./authentication/passport')(passport)
@@ -25,8 +27,11 @@ app.set('port', process.env.PORT || 5000)
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('./public'))
 
 app.set('view engine', 'hbs')
+app.locals.layout = '/layouts/main.hbs'
+app.locals.cache = false
 hbs.registerPartials(__dirname + '/views/partials')
 
 app.use(session({ secret: 'session-secret-stuff', saveUninitialized: true, resave: true }))
@@ -35,10 +40,10 @@ app.use(passport.session())
 app.use(flash())
 
 // routes
-require('./routes.js')(app, passport)
+routes(app, passport)
 
 // socket.io
-require('./sockets.js')(io)
+sockets(io)
 
 // start up
 http.listen(app.get('port'), function(){
